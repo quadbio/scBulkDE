fc7e85ec518f8f0d9395f7bfb04be60c95b7d232
Adnan Abouelela <adnan.abouelela@gmail.com>
2026-01-27 09:36:08 +0100

    update inits

--- src/scbulkde/pp/__init__.py
+++ src/scbulkde/pp/__init__.py
@@ -1,5 +1,5 @@
 """Preprocessing functions."""
 
-from .pp_basic import pseudobulk
+from .pp_basic import _aggregate_counts, _get_aggregation_function, pseudobulk
 
-__all__ = ["pseudobulk"]
+__all__ = ["pseudobulk", "_aggregate_counts", "_get_aggregation_function"]
--- src/scbulkde/tl/__init__.py
+++ src/scbulkde/tl/__init__.py
@@ -1,9 +1,9 @@
 """Tools for differential expression analysis."""
 
-from ._rank_genes_groups import rank_genes_groups
+# from ._rank_genes_groups import rank_genes_groups
 from .tl_basic import de
 
 __all__ = [
-    "rank_genes_groups",
-    "de",
+    # "rank_genes_groups",
+    "de"
 ]
